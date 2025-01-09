7f7bff5a67e2c6fa38b14702f6f73b4ecd08e813
fikri-taufiqurrahman <fikritaufiqurrahman7@gmail.com>
2025-01-09 21:38:00 +0700

    update cors

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,43 +1,60 @@
+const cors = require("cors");
 const nodemailer = require("nodemailer");
 
+// Middleware helper untuk serverless
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
 module.exports = async (req, res) => {
+  // Terapkan CORS
+  await runMiddleware(
+    req,
+    res,
+    cors({
+      origin: "localhost", // Atur asal yang diizinkan
+      methods: ["POST"], // Metode yang diizinkan
+      allowedHeaders: ["Content-Type"],
+    })
+  );
+
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
 
   const { to, subject, text } = req.body;
 
-  // Validasi input dari frontend
   if (!to || !subject || !text) {
     return res
       .status(400)
       .json({ message: "Missing required fields: to, subject, text" });
   }
 
-  // Konfigurasi transporter Nodemailer
   const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE, // Contoh: 'gmail'
+    service: process.env.EMAIL_SERVICE,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
 
-  // Konfigurasi email
-  const mailOptions = {
-    from: process.env.EMAIL_USER, // Email pengirim (dari .env)
-    to, // Email tujuan dikirim dari frontend
-    subject,
-    text,
-  };
-
   try {
-    // Kirim email
-    await transporter.sendMail(mailOptions);
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text,
+    });
     return res.status(200).json({ message: "Email sent successfully" });
   } catch (error) {
     console.error("Error sending email:", error);
-
     return res
       .status(500)
       .json({ message: "Internal Server Error", error: error.message });
